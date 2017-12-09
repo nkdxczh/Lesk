99d7ad400ecfe679989010a75f9421c72ee6e095
nkdxczh <zhc416@lehigh.edu>
2017-12-09 16:19:50 -0500

    finish

--- run_java.sh
+++ run_java.sh
@@ -3,5 +3,5 @@
 for f in `ls data/semcor_txt/*.txt`
 do
 	echo $f
-	java -cp jars/stanford-corenlp-3.8.0.jar:jars/edu.mit.jwi_2.4.0.jar:jars/stanford-corenlp-full-2017-06-09/stanford-corenlp-3.8.0-models.jar:bin/ Lesk $f > results/metrics.txt
+	java -cp jars/stanford-corenlp-3.8.0.jar:jars/edu.mit.jwi_2.4.0.jar:jars/stanford-corenlp-full-2017-06-09/stanford-corenlp-3.8.0-models.jar:bin/ Lesk $f >> results/metrics.txt
 done
